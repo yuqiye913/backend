19c6826e71008d6e6e08059176c2efc6c70c5af1
yuqiye913 <yuqiye2617@gmail.com>
2025-08-21 20:51:13 -0400

    Fix Spring Boot JPA auto-configuration exclusion issue - enable JPA repositories for local development

--- src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
+++ src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
@@ -7,13 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.session.SessionAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableAsync
 @EnableScheduling
 @Import(OpenAPIConfiguration.class)
